ea234a51e23b5c7b928c66cccd03476c5a3fc33f
Andrei <matyshenko-1989@tut.by>
2022-11-09 13:01:15 +0300

    End

--- lesson2/blog/models.py
+++ lesson2/blog/models.py
@@ -4,5 +4,11 @@ class Blog (models.Model):
 
     blog_title = models.CharField(max_length=100)
     blog_date = models.DateTimeField()
-    blog_text = models.CharField(max_length=300)
+    blog_text = models.CharField(max_length=500)
     blog_image = models.ImageField(upload_to='blog_images')
+
+    def get_summary(self):
+        return self.blog_text[:70]
+
+    def __str__(self):
+        return self.blog_title
--- lesson2/blog/views.py
+++ lesson2/blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
+
+def showblog(request):
+    posts = Blog.objects
+    return render(request, 'blog/blog.html', {'posts': posts})
+
 
 # Create your views here.
+def specific_post(request, post_id):
+    post = get_object_or_404(Blog, pk=post_id)
+    return render(request, 'blog/specific_post.html', {'post': post})
